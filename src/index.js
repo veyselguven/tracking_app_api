67d8d0ca89e9a2bbc5a35aedfb06c60abab7f0fd
veyselguven <vgbasbaydar@gmail.com>
2023-08-06 00:53:46 -0700

    initilazed project

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const mongoUri =
+  "mongodb+srv://vgbasbaydar:veyselguven@cluster0.p1dlz3l.mongodb.net/";
+
+mongoose.connect(mongoUri);
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongo instance");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Error connection to mongo", err);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hi thereeee!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on 3000");
+});
