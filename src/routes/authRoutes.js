92f6320cde5952e8a28574a34f165d7884adf9aa
veyselguven <vgbasbaydar@gmail.com>
2023-08-07 17:40:21 -0700

    hash,jwt are done

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -10,13 +10,29 @@ router.post("/signup", async (req, res) => {
   try {
     const user = new User({ email, password });
     await user.save();
-    //console.log(req.body);
-
+    //console.log(req.body);\
     const token = jwt.sign({ userId: user._id }, "MY_SCREET_KEY");
-
     res.send({ token: token });
   } catch (err) {
     return res.status(422).send(err.message);
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).send({ error: "Must provide email and password" });
+  }
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "MY_SCREET_KEY");
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: "Invalid password or email" });
   }
 });
 
