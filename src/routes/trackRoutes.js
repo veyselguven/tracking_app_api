d2c695897ffcf90dcf7eac97149dbb6ab37d0eac
veyselguven <vgbasbaydar@gmail.com>
2023-08-08 19:01:06 -0700

    created new schema

---
+++ src/routes/trackRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const requireAuth = require("../middlewares/requireAuth");
+
+const Track = mongoose.model("Track");
+
+const router = express.Router();
+
+router.use(requireAuth);
+
+router.get("/tracks", async (req, res) => {
+  const tracks = await Track.find({ userId: req.user._id });
+
+  res.send(tracks);
+});
+
+router.post("/tracks", async (req, res) => {});
+
+module.exports = router;
